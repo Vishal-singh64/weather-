559dc95dd45d504e0ea1fc2d3bddc686c9e1ed5b
Vishal-singh64 <87966667+Vishal-singh64@users.noreply.github.com>
2021-10-09 19:04:53 +0530

    Delete Weather.js

--- Weather.js
+++
@@ -1,7 +0,0 @@
-var weatherData=async () =>{
-  const rawData= await fetch(`http://api.openweathermap.org/data/2.5/weather?q=ludhiana&appid=6c012cb514923fae6d816d7b4af1cb73`)
-  let readyData= await rawData.json()
-  console.log(readyData)
-  
-} 
-weatherData()
